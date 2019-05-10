151a9a6b37f32d823bc530a8bf3c3dbbf36a2b88
wafeee <wafefarhani@gmail.com>
2019-05-10 05:23:21 +0100

    Last push

--- insurance/insurance-ejb/src/main/java/tn/esprit/insurance/entity/CarRange.java
+++ insurance/insurance-ejb/src/main/java/tn/esprit/insurance/entity/CarRange.java
@@ -2,7 +2,7 @@ package tn.esprit.insurance.entity;
 
 public enum CarRange {
 	Mercedes , Audi  , AlphaRomeo , BMW , Dacia , Fiat , Hona , Hyundai , Innfinity ,
-	Kia ,  Mazda , Nissan , Peugeot , Renault , Seat , Skoda , Toyota , Volkswagen , Isuzu , 
+	Kia , Mazda , Nissan , Peugeot , Renault , Seat , Skoda , Toyota , Volkswagen , Isuzu , 
 	Daf  
 
 }
